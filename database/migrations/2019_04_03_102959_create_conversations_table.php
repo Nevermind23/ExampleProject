2cb919f9cd533d11460dd74238b78faba4f9f13f
nevermind23 <tyeshela7@gmail.com>
2019-04-03 14:37:22 +0400

    Migrations + Auth

---
+++ database/migrations/2019_04_03_102959_create_conversations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_1_id');
+            $table->unsignedBigInteger('user_2_id');
+            $table->timestamps();
+
+            $table->foreign('user_1_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('user_2_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversations');
+    }
+}
